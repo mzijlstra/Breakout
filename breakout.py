e6213a591faae655cdc5d3e29d16d93f62b024f4
Michael Zijlstra <mzijlstra@gmail.com>
2013-10-13 22:03:50 -0500

    2nd version of the code, not sure what the improvements were

--- breakout.py
+++ breakout.py
@@ -1,110 +1,192 @@
 import pygame, os, sys, random
 
+class Ball(pygame.Rect):
+	"""Ball class for use in Pong and Breakout"""
+	img = pygame.image.load('ball4.png')
+
+	def __init__(self, x=0.0, y=0.0):
+		"""Constructor: loads image, initializes position and speed 
+		optional x and y parameters set initial position"""
+
+		# run super class constructor first
+		rect = Ball.img.get_rect()
+		pygame.Rect.__init__(self, x, y, rect.w, rect.h)
+
+		# precision position (floats)
+		self.px = x
+		self.py = y
+
+		# speed
+		self.dx = 0.0
+		self.dy = 0.0
+
+	def update(self):
+		'updated ball position based on speed'
+		self.px += self.dx
+		self.py += self.dy
+
+		# also update interal rect
+		self.x = self.px
+		self.y = self.py
+
+	def display(self, surface):
+		'Blits ball image to the give surface'
+		surface.blit(Ball.img, self)
+
+	def flipDx(self):
+		"Flips the ball's horizontal speed from pos to neg or visa versa"
+		self.dx = -self.dx
+
+	def flipDy(self):
+		"Flips the ball's vertical speed from pos to neg or visa versa"
+		self.dy = -self.dy
+
+	def goUp(self):
+		"Ensures that dy is set to make ball go up"
+		self.dy = -abs(self.dy)
+
+class Paddle(pygame.Rect):
+	img = pygame.image.load('paddle.png')
+
+	def __init__(self, x=0.0, y=0.0):
+		"""Constructor: loads image, initializes position and speed 
+		optional x and y parameters set initial position"""
+
+		# run super class constructor first
+		rect = Paddle.img.get_rect()
+		pygame.Rect.__init__(self, x, y, rect.w, rect.h)
+
+		# precision position (floats)
+		self.px = x
+		self.py = y
+
+		# speed
+		self.speed = 2
+	
+	def moveLeft(self):
+		'Moves paddle left'
+		self.px -= self.speed
+		self.x = self.px
+
+	def moveRight(self):
+		'Moves paddle right'
+		self.px += self.speed
+		self.x = self.px
+
+	def display(self, surface):
+		'Blits paddle image to the given surface'
+		surface.blit(Paddle.img, self)
+
+class Brick(pygame.Rect):
+	img = pygame.image.load('brick.png')
+
+	def display(self, surface):
+		'Blits brick image to the given surface'
+		surface.blit(Brick.img, self)
+
+
 def main():
-    os.environ['SDL_VIDEO_CENTERED'] = '1'
-    pygame.init()
+	# basic init
+	os.environ['SDL_VIDEO_CENTERED'] = '1'
+	pygame.init()
 
-    size = width, height = 640, 480
-    ballspeed = [2, 2]
-    black = 0, 0, 0
+	# general vars
+	black = 0, 0, 0
+	size = width, height = 640, 480
+	screen = pygame.display.set_mode(size)
+	clock = pygame.time.Clock()
+	state = 'new'# either new or playing
 
-    screen = pygame.display.set_mode(size)
+	# game elements
+	paddle = Paddle(width / 2 - 16, height - 20)
+	ball = Ball()
+	bricks = []
+	brickrect = Brick.img.get_rect()
 
-    #images and rectangles
-    ballimg= pygame.image.load("ball4.png")
-    ball = ballimg.get_rect()
+	# create brick positions
+	rows = 4
+	cols = width / brickrect.width
+	brickW = brickrect.width
+	brickH = brickrect.height
+	
+	for row in range(rows):
+		for col in range(cols):
+			x = col * brickW
+			y = 64 + row * brickH
+			bricks.append(Brick(x, y, brickW, brickH))
 
-    paddleimg = pygame.image.load("paddle.png")
-    paddle = paddleimg.get_rect()
-    paddle.left = (width / 2) - 16
-    paddle.top = height - 20
+	# game loop
+	while True:
 
-    brickimg = pygame.image.load('brick.png')
-    brickrect = brickimg.get_rect()
-    bricks = []
-
-    # create brick positions
-    rows = 4
-    cols = width / brickrect.width
-    brickW = brickrect.width
-    brickH = brickrect.height
-    
-    for row in range(rows):
-        for col in range(cols):
-            x = col * brickW
-            y = 64 + row * brickH
-            bricks.append(pygame.Rect(x, y, brickW, brickH))
-
-    #game clock
-    clock = pygame.time.Clock()
-
-    # game state is either new or playing
-    state = 'new'
-
-    while True:
-
-        # quit on window close or escape key
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT \
-               or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                quit_game()
-                sys.exit()
+		# quit on window close or escape key
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT \
+			   or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+				pygame.quit()
+				sys.exit()
 
 
-        #move paddle based on left and right arrows
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and paddle.left > 0:
-            paddle = paddle.move([-2, 0])
-        if keys[pygame.K_RIGHT] and paddle.right < width:
-            paddle = paddle.move([2, 0])
+		# move paddle based on left and right arrows
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_LEFT] and paddle.left > 0:
+			paddle.moveLeft()
+		if keys[pygame.K_RIGHT] and paddle.right < width:
+			paddle.moveRight()
 
-        if state == 'playing':
-            #move ball, and bounce it against top / left / right (not bottom)
-            ball = ball.move(ballspeed)
-            if ball.left < 0 or ball.right > width:
-                ballspeed[0] = -ballspeed[0]
-            if ball.top < 0: # or ball.bottom > height:
-                ballspeed[1] = -ballspeed[1]
-                
-            # if we hit the paddle
-            if ball.colliderect(paddle):
-                ballspeed[1] = -ballspeed[1]
-                ballspeed[0] = random.randint(-3,3)
-                # make sure we exit the paddle right away
-                ball.move(ballspeed)
+		if state == 'playing':
+			# move ball based on it's own dx/dy
+			ball.update()
 
-            # ball goes off screen at the bottom / change to new ball
-            if ball.bottom > height:
-                state = 'new'
+			# bounce against screen top, left and right 
+			if ball.left < 0 or ball.right > width:
+				ball.flipDx()
+			if ball.top < 0: # or ball.bottom > height:
+				ball.flipDy()
+				
+			# if we hit the paddle
+			if ball.colliderect(paddle):
+				ball.goUp()
+				# random dx after paddle hit
+				ball.dx = random.uniform(-3,3)
+				# make sure we exit the paddle right away
+				ball.update()
 
-            # check block collisions
-            bindex = ball.collidelist(bricks)
-            if bindex != -1:
-                bricks.pop(bindex)
-                ballspeed[1] = -ballspeed[1]
+			# ball goes off screen at the bottom / change to new ball
+			if ball.bottom > height:
+				state = 'new'
 
-        else: # state is new
-            ball.left = paddle.left + 12
-            ball.top = paddle.top - 8
+			# check block collisions
+			bindex = ball.collidelist(bricks)
+			if bindex != -1:
+				bricks.pop(bindex)
+				ball.flipDy()
 
-            if keys[pygame.K_SPACE]:
-                ballspeed[1] = -2
-                ballspeed[0] = random.randint(-3,3)
-                state = 'playing'
-            
+		else: # state is new
+			ball.left = paddle.left + 12
+			ball.top = paddle.top - 8
 
-        #draw screen
-        screen.fill(black)
-        screen.blit(ballimg, ball)
-        screen.blit(paddleimg, paddle)
-        for b in bricks:
-            screen.blit(brickimg, b)
-        pygame.display.flip()
+			if keys[pygame.K_SPACE]:
+				#ball goes up, random angle
+				ball.dy = -2
+				ball.dx = random.uniform(-3, 3)
 
-        #do all this at 40 fps
-        clock.tick(40)
+				#prepare precision movement
+				ball.px = ball.x
+				ball.py = ball.y
 
-def quit_game():
-    pygame.quit()
-    
+				#go to playing state
+				state = 'playing'
+			
+
+		#draw screen
+		screen.fill(black)
+		ball.display(screen)
+		paddle.display(screen)
+		for b in bricks:
+			b.display(screen)
+		pygame.display.flip()
+
+		#do all this at 40 fps
+		clock.tick(40)
+
 if __name__ == '__main__': main()
